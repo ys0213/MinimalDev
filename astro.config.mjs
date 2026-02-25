902095d3d3c29cabc72e9698cdbcc67211ee364c
ys <ys@email.com>
2026-02-25 21:27:31 +0900

    260225_2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     //site: "https://minimaldev.com",
-    site: "https://ys0213.github.io/minimaldev",
+    site: "https://ys0213.github.io/MinimalDev",
     base: "/MinimalDev",
     integrations: [sitemap()],
 });
